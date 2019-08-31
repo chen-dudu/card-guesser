069ef060663cf7b8808ffbd9a47b12f03c39cdea
unknown <43197141+chen-dudu@users.noreply.github.com>
2019-09-01 00:07:55 +1000

    completed the simplest version of the guessing algorithm

--- Proj1.hs
+++ Proj1.hs
@@ -1,30 +1,16 @@
 
 
 
--- module Proj1 (feedback, initialGuess, nextGuess, GameState) where
-module Proj1 (initialGuess, feedback) where
+module Proj1 (feedback, initialGuess, nextGuess, GameState) where
 
 import Data.List
 import Data.Ord (comparing)
 import Card
 
-type GameState = [[Card]]
-
 rankchars = "23456789TJQKA"
 suitchars = "CDHS"
 
-initialGuess :: Int -> ([Card], GameState)
-initialGuess n
-    | n < 1 = error("invalid input!")
-    | otherwise = (map stringToCard (zipWith formCardString ranks suits), [[]])
-        where ranks = initialChooseRank separation rankchars
-              suits = [suitchars !! i | i <- [0..n]]
-              separation = (length rankchars) `div` (n + 1)
-
-
--- nextGuess :: ([Card], GameState) -> (Int, Int, Int, Int, Int) -> ([Card], GameState)
-
-
+type GameState = [[Card]]
 
 feedback :: [Card] -> [Card] -> (Int, Int, Int, Int, Int)
 feedback target guess = (countMatchCard (sort target) (sort guess),
@@ -33,11 +19,27 @@ feedback target guess = (countMatchCard (sort target) (sort guess),
                          countHigherRank (sortByRank target) (sortByRank guess),
                          countMatchSuit target guess)
 
+initialGuess :: Int -> ([Card], GameState)
+initialGuess n
+    | n < 1 = error("invalid input!")
+    | otherwise = (firstGuess, delete firstGuess (initialGameState n deck))
+        where firstGuess = stringToCard (zipWith formCardString ranks suits)
+              ranks = initialChooseRank separation rankchars
+              suits = [suitchars !! i | i <- [0..n]]
+              separation = (length rankchars) `div` (n + 1)
+              deck = stringToCard [i:j:[] | j <- suitchars, i <- rankchars]
+
+
+nextGuess :: ([Card], GameState) -> (Int, Int, Int, Int, Int) -> ([Card], GameState)
+nextGuess (guess, oldState) oldFeedback = (newState !! 0, newState) 
+    where newState = [i | i <- oldState, (feedback i guess) == oldFeedback]
 
 
 ------------------------------------------------------------------------------
 
 ------------------------------ Helper Functions ------------------------------
+
+------------------------------------------------------------------------------
 
 -- given a list, and an integer indicating the separation width, 
 -- this function returns a new list of elements with the specified width
@@ -48,16 +50,23 @@ initialChooseRank n lst
  | otherwise = [lst !! (i*(n + 1) - 1) | i <- [1..length lst], 
                                          (i*(n + 1) - 1) < length lst]
 
+initialGameState :: Int -> [Card]-> GameState
+initialGameState 2 deck = choose2 deck
+initialGameState 3 deck = choose3 deck
+initialGameState 4 deck = choose4 deck
+initialGameState _ _ = error("invalid input!")
+
 -- this function takes two characters indicating 
 -- the rank and suit of a card, and produces a String 
 -- representation of the Card
 formCardString :: Char -> Char -> String
 formCardString rank suie = rank:suie:[]
 
--- convert the string representation of a card to the actual Card type
-stringToCard :: String -> Card
-stringToCard card = read card :: Card
-
+-- input: a list of cards represented as strings
+-- output: a list of cards with type Card
+stringToCard :: [String] -> [Card]
+stringToCard [] = []
+stringToCard (x:xs) = (read x :: Card) : stringToCard xs
 
 countMatchCard :: [Card] -> [Card] -> Int
 countMatchCard [] [] = 0
@@ -83,6 +92,12 @@ countMatchRank (x:xs) ys
 -- only for pattern matching to be exhaustive
 countMatchRank _ _ = 0
 
+-- countMatchRank :: [Card] -> [Card] -> Int
+-- countMatchRank [] [] = 0
+-- countMatchRank (x:xs) (y:ys)
+--  | equalRank x y = 1 + countMatchRank xs ys
+--  | otherwise = countMatchRank xs ys
+-- countMatchRank _ _ = 0
 
 countHigherRank :: [Card] -> [Card] -> Int
 countHigherRank [] [] = 0
@@ -101,6 +116,49 @@ countMatchSuit (x:xs) ys
 -- only for pattern matching to be exhaustive
 countMatchSuit _ _ = 0
 
+-- countMatchSuit :: [Card] -> [Card] -> Int
+-- countMatchSuit [] [] = 0
+-- countMatchSuit (x:xs) (y:ys)
+--  | equalSuit x y = 1 + countMatchSuit xs ys
+--  | otherwise = countMatchSuit xs ys
+-- countMatchSuit _ _ = 0
+
+
+getIndex :: Maybe Int -> Int
+getIndex Nothing = -1
+getIndex (Just n) = n
+
+choose2 :: [Card] -> [[Card]]
+choose2 lst = [[i, j] | i <- lst, 
+                        j <- (drop (1 + getIndex (elemIndex i lst)) lst)]
+
+choose3 :: [Card] -> [[Card]]
+choose3 lst = [[i, j] | i <- lst, 
+                        j <- (drop (1 + getIndex (elemIndex i lst)) lst), 
+                        k <- (drop (1 + getIndex (elemIndex j lst)) lst)]
+
+choose4 :: [Card] -> [[Card]]
+choose4 lst = [[i, j] | i <- lst, 
+                        j <- (drop (1 + getIndex (elemIndex i lst)) lst), 
+                        k <- (drop (1 + getIndex (elemIndex j lst)) lst), 
+                        m <- (drop (1 + getIndex (elemIndex k lst)) lst)]
+
+
+
+equalFeedback :: (Int, Int, Int, Int, Int) -> [Card] -> [Card] -> Bool
+equalFeedback oldFeedback prevGuess newGuess = 
+    (feedback prevGuess newGuess) == oldFeedback
+
+
+
+allpos = initialGameState 2 (stringToCard [i:j:[] | j <- suitchars, i <- rankchars])
+ans = stringToCard ["2C", "3C"]
+guess1 = stringToCard ["6C", "JD"]
+f1 = (0,2,0,0,1)
+guess2 = stringToCard ["QC", "KC"]  
+f2 = (0,2,0,0,2)
+
+------------------------------------------------------------------------------
 
 -- this function compare the ranks of two cards
 -- return -1 if the rank of the first onen is smaller
@@ -149,7 +207,4 @@ deleteByRank = deleteBy equalRank
 -- similar to the delete funciton defined in Data.List, but based on suit
 deleteBySuit :: Card -> [Card] -> [Card]
 deleteBySuit = deleteBy equalSuit
-
-
-
 
