eeb226267dc489113ee2862a1d328617a96c44c3
unknown <43197141+chen-dudu@users.noreply.github.com>
2019-09-10 21:42:21 +1000

    added comments and re-organised code to make it more readable

--- Proj1.hs
+++ Proj1.hs
@@ -34,7 +34,6 @@ import Card
 --------------------------------- Constants ----------------------------------
 ------------------------------------------------------------------------------
 
--- Some constants used in this module
 -- all ranks in a standard 52-card deck
 allRanks = [R2 .. Ace]
 -- all suit in a standard 52-card deck
@@ -67,38 +66,34 @@ initialGuess :: Int -> ([Card], GameState)
 initialGuess n
     | n < 1 = error("invalid input!")
     | otherwise = (firstGuess, delete firstGuess (initialGameState n deck))
-        where firstGuess = zipWith formCard suits ranks
-              -- rank for the first guess
-              ranks = initialChooseRank separation allRanks
-              -- suit for the first guess
-              suits = [allSuits !! i | i <- [0..n]]
-              -- width of gap between cards in the first guess
-              separation = (length allRanks) `div` (n + 1)
-              -- a standard 52-card deck
-              deck = [firstCard..lastCard]
+        where 
+            -- width of gap between cards in the first guess
+            separation = (length allRanks) `div` (n + 1)
+            -- rank for the first guess
+            ranks = initialChooseRank separation allRanks
+            -- suit for the first guess
+            suits = [allSuits !! i | i <- [0..n]]
+            -- first guess for the game
+            firstGuess = zipWith formCard suits ranks
+            -- a standard 52-card deck
+            deck = [firstCard..lastCard]
 
-nextGuess :: ([Card], GameState) -> (Int, Int, Int, Int, Int) -> ([Card], GameState)
+nextGuess :: ([Card],GameState) -> (Int,Int,Int,Int,Int) -> ([Card],GameState)
 nextGuess (oldGuess, oldState) oldFeedback = (newGuess, newState)
     where
         -- all card lists in the GameState with the 
         -- same feedback could be answer 
-        possibleAns = [i | i <- oldState, (feedback i oldGuess) == oldFeedback]
+        possibleAns = [i | i <- oldState,(feedback i oldGuess) == oldFeedback]
         -- choose the next guess from all possible answers
         newGuess = chooseNextGuess possibleAns
         -- update GameState
         newState = delete newGuess possibleAns
 
-chooseNextGuess :: [[Card]] -> [Card]
-chooseNextGuess lst
-    | length lst >= 1500 = lst !! (div (length lst) 2)
-    | otherwise = newGuess 
-        where feedbackList = [feedback i j | i <- lst, j <- lst]
-              scoreList = [(calculateScore(group $ sort [feedback j i | j <- lst, j /= i]), i) | i <- lst]
-              (_, newGuess) = head(sort scoreList)
-
 ------------------------------------------------------------------------------
 ------------------------------ Helper Functions ------------------------------
 ------------------------------------------------------------------------------
+
+---------------------------- calculating feedback ----------------------------
 
 -- Given two lists of cards, this function returns an integer indicating
 -- the number of matching cards in the two input lists
@@ -116,8 +111,9 @@ countMatchCard _ _ = 0
 countLowerRank :: [Card] -> [Card] -> Int
 countLowerRank [] [] = 0
 countLowerRank (x:xs) (y:ys)
-    -- | (compareRank x y) < 0 = 1 + countLowerRank xs (y:ys)
     | (getRank x) < (getRank y) = 1 + countLowerRank xs (y:ys)
+    -- the lowest rank in the answer is higher than the lowest 
+    -- rank in the guess, no more comparison is needed
     | otherwise = 0 
 -- only for pattern matching to be exhaustive
 countLowerRank _ _ = 0
@@ -140,8 +136,10 @@ countHigherRank [] [] = 0
 countHigherRank [] _ = 0
 countHigherRank _ [] = 0
 countHigherRank xs ys
-    -- | (compareRank (last xs) (last ys)) > 0 = 1 + countHigherRank (init xs) ys
-    | (getRank (last xs)) > (getRank (last ys)) = 1 + countHigherRank (init xs) ys
+    | (getRank (last xs)) > (getRank (last ys)) 
+        = 1 + countHigherRank (init xs) ys
+    -- the highest rank in the answer is lower than the highest 
+    -- rank in the guessing, no more comparison is needed
     | otherwise = 0
 
 -- Given two lists of cards, this function returns an integer indicating
@@ -153,6 +151,8 @@ countMatchSuit (x:xs) ys
     | otherwise = countMatchSuit xs ys
 -- only for pattern matching to be exhaustive
 countMatchSuit _ _ = 0
+
+----------------------------- making first guess -----------------------------
 
 -- Given an integer indicating the number of cards in the answer, and a list
 -- of cards as the deck, this function returns all the combinations
@@ -171,11 +171,6 @@ initialChooseRank n lst
  | otherwise = [lst !! (i*(n + 1) - 1) | i <- [1..length lst], 
                                          (i*(n + 1) - 1) < length lst]
 
--- Given the suit and rank of a card, this function returns a card
--- with the specified suit and rank
-formCard :: Suit -> Rank -> Card
-formCard suit rank = Card suit rank
-
 choose2 :: [Card] -> [[Card]]
 choose2 lst = [[i, j] | i <- lst, 
                         j <- (drop (1 + getIndex (elemIndex i lst)) lst)]
@@ -191,32 +186,50 @@ choose4 lst = [[i, j, k, m] | i <- lst,
                         k <- (drop (1 + getIndex (elemIndex j lst)) lst), 
                         m <- (drop (1 + getIndex (elemIndex k lst)) lst)]
 
+----------------------------- making next guess ------------------------------
+
+-- Given a list of lists of cards, this function returns a list of card as the
+-- guess for the next guess, using the algoritm provided in the project spec
+chooseNextGuess :: [[Card]] -> [Card]
+chooseNextGuess lst
+    | length lst >= 1500 = lst !! (div (length lst) 2)
+    | otherwise = newGuess 
+        where feedbackList = [feedback i j | i <- lst, j <- lst]
+              scoreList = [(calculateScore(group $ sort 
+                            [feedback j i | j <- lst, j /= i]), i) | i <- lst]
+              (_, newGuess) = head(sort scoreList)
+
 -- Given a list of lists of tuples, this function returns the score 
 -- for the list using the algorithm provided in the project spec
+-- score = (sum (size of Xi)^2) / (sum (size of Xi)), 
+-- Xi is each tuple list in the list.
 calculateScore :: [[(Int, Int, Int, Int, Int)]] -> Double
 calculateScore [] = 0.0
 calculateScore lst = fromIntegral ss / fromIntegral s 
                          where (ss, s) = getSums lst
+
+---------------------------- supporting functions ----------------------------
+
+-- Given the suit and rank of a card, this function returns a card
+-- with the specified suit and rank
+formCard :: Suit -> Rank -> Card
+formCard suit rank = Card suit rank
+
+-- Given a card, this funciton returns the rank of the card
+getRank :: Card -> Rank
+getRank (Card _ rank) = rank
 
 -- Given a (Maybe Int) type, this funciton returns the integer (-1 for Nothing)
 getIndex :: Maybe Int -> Int
 getIndex Nothing = -1
 getIndex (Just n) = n
 
--- Given a card, this funciton returns the rank of the card
-getRank :: Card -> Rank
-getRank (Card suit rank) = rank
-
--- Given two cards, this function check if they have the same rank
--- this function checks if two cards have the same rank
--- used as an equality test function for deleteBy
-equalRank :: Card -> Card -> Bool
-equalRank (Card _ rank1) (Card _ rank2) = rank1 == rank2
-
--- this function checks if two cards have the same suit
--- used as an equality test function for deleteBy
-equalSuit :: Card -> Card -> Bool
-equalSuit (Card suit1 _) (Card suit2 _) = suit1 == suit2
+-- Given a list of lists of tuples, this function returns a 2-tuple ---
+-- the first component is the sum of square of each tuple list
+-- the second component is the sum of each tuple list
+getSums :: [[(Int, Int, Int, Int, Int)]] -> (Int, Int)
+getSums [] = (0, 0)
+getSums (x:xs) = (ss + (length x)^2, s + length x) where (ss, s) = getSums xs
 
 -- similar to the elem function defined in the Prelude, but based on rank
 elemRank :: Card -> [Card] -> Bool
@@ -244,6 +257,13 @@ deleteByRank = deleteBy equalRank
 deleteBySuit :: Card -> [Card] -> [Card]
 deleteBySuit = deleteBy equalSuit
 
-getSums :: [[(Int, Int, Int, Int, Int)]] -> (Int, Int)
-getSums [] = (0, 0)
-getSums (x:xs) = (ss + (length x)^2, s + length x) where (ss, s) = getSums xs
+-- Given two cards, this function check if they have the same rank
+-- this function checks if two cards have the same rank
+-- used as an equality test function for deleteBy
+equalRank :: Card -> Card -> Bool
+equalRank (Card _ rank1) (Card _ rank2) = rank1 == rank2
+
+-- this function checks if two cards have the same suit
+-- used as an equality test function for deleteBy
+equalSuit :: Card -> Card -> Bool
+equalSuit (Card suit1 _) (Card suit2 _) = suit1 == suit2
